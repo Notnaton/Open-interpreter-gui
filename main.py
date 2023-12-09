2f85f3628f357cc79e72a457a6ea5ee9e0fe8d88
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2023-12-09 20:51:44 +0100

    Minor tweak to wrapping

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ for i, (message, is_user) in enumerate(messages):
     # Create the label with blue background inside the frame
     bubble = customtkinter.CTkLabel(master=chat_frame,
                                     text=message, 
-                                    wraplength=250,
+                                    wraplength=300,
                                     bg_color="transparent",  # Blue background
                                     corner_radius=10, # Apply rounded corners
                                     fg_color="#144870")
