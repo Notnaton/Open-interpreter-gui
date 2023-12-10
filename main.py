0ab195e7228f8de2626f695f94863089d19004d9
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2023-12-10 13:20:36 +0100

    Loading messages from history

--- main.py
+++ main.py
@@ -1,82 +1,140 @@
 from pathlib import Path
 import customtkinter
 import interpreter
+import json
 
-interpreter.model = "openai/local"
-interpreter.api_base = "http://localhost:1234/v1"
+class HistoryFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, path):
+        super().__init__(master)
+        self.history_chats = {}
+        for file in path.iterdir():
+            # Extract the filename from the path using pathlib
+            filename = file.name
+            # Split the filename at the first double underscore '__'
+            key = filename.split('__')[0].replace('_', ' ')
+            # Add to the dictionary
+            self.history_chats[key] = file
 
-path = Path(interpreter.conversation_history_path)
-history_chats = {}
+        self.history_frame = customtkinter.CTkScrollableFrame(master=master)
+        self.history_frame.grid(row=0, column=0, padx=(10, 0), pady=10, sticky="nsw", rowspan=2)
 
-for file in path.iterdir():
-    # Extract the filename from the path using pathlib
-    filename = file.name
-    # Split the filename at the first double underscore '__'
-    key = filename.split('__')[0].replace('_', ' ')
-    # Add to the dictionary
-    history_chats[key] = file
+        self.history_label = customtkinter.CTkLabel(master=self.history_frame, text="Chat history", font=("default", 20))
+        self.history_label.grid(row=0, column=0, padx=20, pady=(0,0), sticky="nw")
 
-def button_callback():
-    print("button pressed")
+        self.history_buttons = []
+        self.update_signal = False
+        self.message_data = []
 
-def load_history(path):
-    print(path)
-    pass
+        for i, (key, path) in enumerate(self.history_chats.items()):
+            # Use a lambda function to pass the specific path as an argument to load_history
+            history_button = customtkinter.CTkButton(master=self.history_frame, text=key, command=lambda p=path: self.load_history(p))
+            history_button.grid(row=i+1, column=0, padx=0, pady=(10,0), sticky="we")
+            self.history_buttons.append(history_button)
+        
+    def load_history(self, path):
+        print(f"\n{path}")
+        with open(path, "r") as f:
+            data = json.load(f)
+            self.message_data = data
+            self.update_signal = True
 
-app = customtkinter.CTk()
-app.title("Open Interpreter")
-app.geometry("600x480")
-app.grid_columnconfigure(1, weight=1)
-app.grid_rowconfigure(0, weight=5)
+class ChatFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.chat_frame = customtkinter.CTkScrollableFrame(master=master)
+        self.chat_frame.grid(row=0, column=1, pady=10, padx=10, sticky="nsew")
+        self.chat_frame.grid_columnconfigure(0, weight=1)
 
-history_frame = customtkinter.CTkScrollableFrame(master=app)
-history_frame.grid(row=0, column=0, padx=(10, 0), pady=10, sticky="nsw", rowspan=2)
+        self.chat_bubbles = []
 
-history_label = customtkinter.CTkLabel(master=history_frame, text="Chat history", font=("default", 20))
-history_label.grid(row=0, column=0, padx=20, pady=(0,0), sticky="nw")
+        
+        self.input_frame = customtkinter.CTkFrame(master=master)
+        self.input_frame.grid(row=1, column=1, pady=(0,10), padx=10, sticky="swe")
+        self.input_frame.grid_columnconfigure(0, weight=1)
 
-history_buttons = []
+        self.user_input = customtkinter.CTkTextbox(master=self.input_frame, height=40)
+        self.user_input.grid(row=0,column=0, padx=10,pady=10, sticky="nswe")
 
-for i, (key, path) in enumerate(history_chats.items()):
-    # Use a lambda function to pass the specific path as an argument to load_history
-    history_button = customtkinter.CTkButton(master=history_frame, text=key, command=lambda p=path: load_history(p))
-    history_button.grid(row=i+1, column=0, padx=0, pady=(10,0), sticky="we")
-    history_buttons.append(history_button)
+        self.user_input_button = customtkinter.CTkButton(master=self.input_frame, text=">", command=print("click"), width=50)
+        self.user_input_button.grid(row=0, column=1, padx=(0,10), pady=10, sticky="nse")
+    
+    def render_messages(self, messages):
+        
+        # First, clear existing messages
+        for widget in self.chat_bubbles:
+            widget.destroy()
+        self.chat_bubbles.clear()
 
-chat_frame = customtkinter.CTkScrollableFrame(master=app)
-chat_frame.grid(row=0, column=1, pady=10, padx=10, sticky="nsew")
-chat_frame.grid_columnconfigure(0, weight=1)
+        render_messages = []
+        for message in messages:
+            is_user = message["role"] == "user" or False
+            if "message" in message.keys():
+                render_messages.append((message["message"], is_user))
 
-# Sample data: List of tuples with (message, is_user) 
-# Replace this with your actual chat data
-messages = [("Hello, how are you? we need a longer message to see what happens when it overflows and \nchanges to new line", False),
-            ("I'm fine, thank you!", True),
-            ("Great to hear that!", False)]
+        for i, (message, is_user) in enumerate(render_messages):
+            # Create the label with blue background inside the frame
+            bubble = customtkinter.CTkLabel(master=self.chat_frame,
+                                            text=message, 
+                                            wraplength=300,
+                                            justify="left",
+                                            bg_color="transparent",  # Blue background
+                                            corner_radius=10, # Apply rounded corners
+                                            fg_color="#144870")
+            # Grid the frame with appropriate padding and alignment
+            padx, pady = 10, 10
+            sticky = "e" if is_user else "w"
+            bubble.grid(row=i, column=0, padx=padx, pady=pady, sticky=sticky)
 
-for i, (message, is_user) in enumerate(messages):
-    # Create the label with blue background inside the frame
-    bubble = customtkinter.CTkLabel(master=chat_frame,
-                                    text=message, 
-                                    wraplength=300,
-                                    justify="left",
-                                    bg_color="transparent",  # Blue background
-                                    corner_radius=10, # Apply rounded corners
-                                    fg_color="#144870")
+            self.chat_bubbles.append(bubble)
 
-    # Grid the frame with appropriate padding and alignment
-    padx, pady = 10, 5
-    sticky = "e" if is_user else "w"
-    bubble.grid(row=i, column=0, padx=padx, pady=pady, sticky=sticky)
+        # Update the UI
+        self.update()
 
-input_frame = customtkinter.CTkFrame(master=app)
-input_frame.grid(row=1, column=1, pady=(0,10), padx=10, sticky="swe")
-input_frame.grid_columnconfigure(0, weight=1)
+        # Update the scroll region
+        if hasattr(self.chat_frame, '_parent_canvas'):
+            canvas = self.chat_frame._parent_canvas
+            canvas.config(scrollregion=canvas.bbox("all"))
+        self.scroll_to_bottom()
 
-user_input = customtkinter.CTkTextbox(master=input_frame, height=40)
-user_input.grid(row=0,column=0, padx=10,pady=10, sticky="nswe")
+    def scroll_to_bottom(self):
+        # Try to access the internal canvas and use yview_moveto
+        if hasattr(self.chat_frame, '_parent_canvas'):
+            self.chat_frame.update_idletasks()  # Update the UI
+            canvas = self.chat_frame._parent_canvas
+            canvas.yview_moveto(1.0)
 
-user_input_button = customtkinter.CTkButton(master=input_frame, text=">", command=print("click"), width=50)
-user_input_button.grid(row=0, column=1, padx=(0,10), pady=10, sticky="nse")
+class InterpreterApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Open Interpreter")
+        self.geometry("600x480")
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=5)
 
+        self.interpreter = interpreter
+        self.interpreter.model = "openai/local"
+        self.interpreter.api_base = "http://localhost:1234/v1"
 
-app.mainloop()
+        self.path = Path(self.interpreter.conversation_history_path)
+
+        #self.messages = []
+        self.update_messages = False
+        
+        self.history_frame = HistoryFrame(self, self.path)
+        self.chat_frame = ChatFrame(self)
+
+        self.check_update_signal()
+    
+    def check_update_signal(self):
+        if self.history_frame.update_signal:
+            messages = self.history_frame.message_data
+            print(messages)
+            self.chat_frame.render_messages(messages)
+            self.history_frame.update_signal = False
+        self.after(500, self.check_update_signal)
+
+        
+
+if __name__ == "__main__":
+    app = InterpreterApp()
+    app.mainloop()
